267aed027d5edf9a6692132a8b13da5f41d3abba
Vo Trung Hieu <votrunghieu4@dtu.edu.vn>
2026-04-03 06:35:20 +0700

    second

---
+++ TestGit/Demo1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    public class Demo1
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
