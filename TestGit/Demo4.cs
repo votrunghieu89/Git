c23d3dd06ad01d3662df0b36270a6e56b0d2cb35
Vo Trung Hieu <votrunghieu4@dtu.edu.vn>
2026-04-03 07:04:59 +0700

    six

---
+++ TestGit/Demo4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    public class Demo4
+    {
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be zero.");
+            }
+            return a / b;
+        }
+    }
+}
