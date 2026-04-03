5800656d1a26df37bc579936606874bab38c6b18
Vo Trung Hieu <votrunghieu4@dtu.edu.vn>
2026-04-03 08:50:46 +0700

    seven

---
+++ TestGit/Demo5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    public class Demo5
+    {
+        public int Modulo(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot perform modulo by zero.");
+            }
+            return a % b;
+        }
+    }
+}
